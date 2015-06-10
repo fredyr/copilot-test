ed3b864add820bc4599f97ac6e3169d5e07c07f5
KarlJohan <karl-johan.hultin@labgruppen.com>
2015-06-10 08:41:31 +0200

    Raw ADC values converted to temperature using 3 look-up tables.

--- temp.hs
+++ temp.hs
@@ -15,10 +15,19 @@ sensorDataTest = Just sensorData
 sensorDataArray :: Stream Word32 -> Stream Word32
 sensorDataArray idx = externArray "temp_sensor_data" idx 8 sensorDataTest
 
-ntcPSU_lut :: Stream Word32 -> Stream Int32
-ntcPSU_lut idx = externArray "NTC_PSU_lut" idx 17 ntcPSU_lut_test
 
-ntcPSU_lut_test = (Just $ repeat [162070, 136700, 115598, 98282, 84266, 73065, 64197, 57175, 51517, 46737, 42351, 37875, 32825, 26716, 19063, 9382, -2809])
+ntcPSU_lut :: Stream Word32 -> Stream Float
+ntcPSU_lut idx = externArray "NTC_PSU_lut" idx 17 ntcPSU_lut_test
+ntcPSU_lut_test = (Just $ repeat [185.00, 141.86, 115.66, 95.57, 83.00, 73.66, 65.99, 59.32, 53.28, 47.60, 42.13, 36.73, 31.15, 25.07, 18.76, 10.82, -4.42])
+
+ntcAMP_lut :: Stream Word32 -> Stream Float
+ntcAMP_lut idx = externArray "NTC_PSU_lut" idx 17 ntcAMP_lut_test
+ntcAMP_lut_test = (Just $ repeat [178.00, 149.00, 128.57, 112.68, 100.97, 91.56, 83.47, 76.19, 69.37, 62.74, 56.08, 49.16, 41.65, 32.97, 21.92, 4.20, -85.40])
+
+ntcAMBIENT_lut :: Stream Word32 -> Stream Float
+ntcAMBIENT_lut idx = externArray "NTC_PSU_lut" idx 17 ntcAMBIENT_lut_test
+ntcAMBIENT_lut_test = (Just $ repeat [147.68, 97.16, 73.74, 60.56, 51.19, 43.77, 37.48, 31.90, 26.77, 21.91, 17.19, 12.48, 7.64, 2.51, -3.17, -9.84, -18.54])
+
 
 -- CHIP TEMP SENSORS
 chipTempInternal :: Stream Word32
@@ -39,20 +48,23 @@ adcTempChanB = sensorDataArray (constW32 3)
 adcRawTempChanB :: Stream Word32
 adcRawTempChanB = sensorDataArray (constW32 3)
 
-adcRawTempSplit :: Stream Word32 -> (Stream Word32, Stream Word32)
+adcRawTempSplit :: Stream Word32 -> (Stream Word32, Stream Float)
 adcRawTempSplit raw = (idx, offset)
   where
-    idx = raw .>>. (constW32 8)
-    offset = raw .&. 0xff
+    -- Input from the ADC is 12 bits.
+    -- The top 4 bits are used as an index in a lockup table.
+    -- The last 8 bits are converted to float and used for interpolation.
+    rawC = U.clamp 0 4095 raw
+    idx = rawC .>>. constW32 8
+    offset' = unsafeCast $ rawC .&. 0xff
+    offset = offset' / 256.0
 
-adcRawTransfer :: (Stream Word32, Stream Word32) -> Stream Int32
-adcRawTransfer (idx, offset) = cast y
+adcRawTransform :: (Stream Word32 -> Stream Float) -> (Stream Word32, Stream Float) -> Stream Float
+adcRawTransform lut (idx, offset) = y
   where
-    y1 = ntcPSU_lut idx
-    y0 = ntcPSU_lut (idx+1)
-    --offset' = cast offset
-    offset' = 3
-    y = y0 + ((y1 - y0) * offset') `div` 256
+    y0 = lut idx
+    y1 = lut (idx+1)
+    y = y0 + ((y1 - y0) * offset)
 
 adcTemps = foldr1 (||) chans
   where
@@ -60,13 +72,13 @@ adcTemps = foldr1 (||) chans
     f = ((U.hysteresis 700 1000) . sensorDataArray . constW32)
 
 s :: Stream Word32
-s = extern "s" (Just [10, 20..])
+s = extern "s" (Just [15, 30..])
 
 t :: Stream Word32
 t = extern "t" (Just [73, 70..])
 
 u :: Stream Word32
-u = extern "t" (Just [300, 350..])
+u = extern "u" (Just [0, 123..])
 
 f :: Stream Float
 f = extern "f" (Just [11.0, 15.7 ..])
@@ -103,10 +115,8 @@ testSpec = do
   --observer "m_hyst" mhyst
   --observer "adcTemps" adcTemps
   -- trigger "s_muteAllChannels" simp [arg shyst]
-  observer "s" s
-  observer "adcRawTransfer" (adcRawTransfer aRSplit)
-  observer "0_adcRawTempSplit_fst" $ fst aRSplit
-  observer "0_adcRawTempSplit_snd" $ snd aRSplit
+  observer "u" u
+
 
   observer "f" f
   observer "fan" $ fanControl f true
@@ -119,8 +129,29 @@ testSpec = do
     simp  = U.impulse shyst
     timp  = U.impulse thyst
     mimp  = U.impulse mhyst
-    aRSplit = adcRawTempSplit s
-    del = div u 123
+    
+
+testSpec_ADCTemp :: Spec
+testSpec_ADCTemp = do
+  observer "0__fast" fast
+  observer "1_TempSplit_fst" $ fst aRSplit_fast
+  observer "2_TempSplit_snd" $ snd aRSplit_fast
+  observer "3_Transform" $ adcRawTransform ntcPSU_lut aRSplit_fast
+
+  observer "4_slow" slow
+  observer "5_TempSplit_fst" $ fst aRSplit_slow
+  observer "6_TempSplit_snd" $ snd aRSplit_slow
+  observer "7_Transform" $ adcRawTransform ntcPSU_lut aRSplit_slow
+  where
+    fast = [0] ++ fast + 123
+    slow = [0] ++ slow + 7
+    aRSplit_fast = adcRawTempSplit fast
+    aRSplit_slow = adcRawTempSplit slow
+
+testSpec_Fan :: Spec
+testSpec_Fan = do
+  observer "f" f
+  observer "fan" $ fanControl f true
 
 tempSpec :: Spec
 tempSpec = do
@@ -132,6 +163,12 @@ tempSpec = do
 
 runTestSpec = do
   interpret 20 testSpec
+
+runTestSpec_Fan = do
+  interpret 20 testSpec_Fan
+
+runTestSpec_ADCTemp = do
+  interpret 40 testSpec_ADCTemp
 
 runTempSpec = do
   interpret 20 tempSpec
